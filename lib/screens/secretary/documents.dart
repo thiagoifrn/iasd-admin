78ecdc278a67fccaff3c1cf0e6d3fd402f31625a
ThiagoIfrn <thiagoifrn@gmail.com>
2023-03-29 14:34:03 -0300

    Adiciona assets/images

---
+++ lib/screens/secretary/documents.dart
@@ -0,0 +1,35 @@
+import 'package:admin/controllers/MenuAppController.dart';
+import 'package:admin/responsive.dart';
+import 'package:admin/screens/dashboard/dashboard_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../main/components/side_menu.dart';
+
+class Documents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: SideMenu(),
+      body: SafeArea(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            // We want this side menu only for large screen
+            if (Responsive.isDesktop(context))
+              Expanded(
+                // default flex = 1
+                // and it takes 1/6 part of the screen
+                child: SideMenu(),
+              ),
+            Expanded(
+              // It takes 5/6 part of the screen
+              flex: 5,
+              child: Container(color: Colors.blue),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
